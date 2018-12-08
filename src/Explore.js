4038a055791d7f03f2ce488de737d6b780b3b5cd
Qin Guan <qinguan20040914@gmail.com>
2018-12-08 18:09:01 +0800

    i want to kill you aakash i really do can u please not fkup AGAIN

--- src/Explore.js
+++ src/Explore.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react'
-import {View, Text, TouchableOpacity, ImageBackground} from 'react-native'
-
+import {View, Text, TouchableOpacity, ImageBackground, SafeAreaView} from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 class Explore extends Component {
   static navigationOptions = {
-    headerTitle: null,
+    header: null,
   }
 
   render() {
@@ -17,6 +17,16 @@ class Explore extends Component {
                            style={{
                              flex: 1, resizeMode: 'contain'
                            }} imageStyle={{opacity: 0.5}}>
+            <SafeAreaView style={{
+              position: 'absolute',
+              marginLeft: 8
+            }}>
+              <TouchableOpacity onPress={() => {
+                this.props.navigation.goBack()
+              }}>
+                <Icon name={'keyboard-arrow-left'} color={'white'} size={40}/>
+              </TouchableOpacity>
+            </SafeAreaView>
           </ImageBackground>
         </View>
     )
