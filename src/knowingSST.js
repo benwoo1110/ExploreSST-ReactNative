aab5c902c90d2119f8bae2e6640aa9c3c5f283c4
AakashSanjayMehta <asrm003@gmail.com>
2018-12-23 23:57:41 +0700

    g

--- src/knowingSST.js
+++ src/knowingSST.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Text, View, ImageBackground, SafeAreaView, TouchableOpacity, Image, ScrollView } from 'react-native'
 import { FILE_URL } from './constants'
 import Icon from 'react-native-vector-icons/MaterialIcons'
-import SpeechBubble from 'react-native-speech-bubble'
+
 
 class KnowingSST extends Component {
   static navigationOptions = {
@@ -33,14 +33,33 @@ class KnowingSST extends Component {
               }}>Rachel</Text>
             </View>
             <ScrollView>
-              <SpeechBubble
-                speeches={["SST conducts digital citizenship workshops for primary schools and I was one of the students who attended.", 
-                "I learnt useful tips on how I can encourage the healthy and responsible use of technology back in my primary school."]}
-              />
 
+              <View style={{
 
-
-
+                width: '100%',
+                backgroundColor: 'white',
+                shadowColor: 'gray',
+                shadowBlur: 10,
+                shadowOffset: {
+                  width: 2,
+                  height: 2
+                },
+                shadowOpacity: 1,
+                borderRadius: 5,
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: 16
+              }}><Text style={{
+                padding: 8,
+                fontSize: 18,
+                fontFamily: 'Avenir Next',
+              }}>SST conducts digital citizenship workshops for primary schools and I was one of the students who attended.
+                      I learnt useful tips on how I can encourage the healthy and responsible use of technology back in my primary school.
+                      I found it amazing that the SST students conducting the workshops also organised sessions for parents of SST as well.
+                      My parents also thought it was important for a STEM school to have a holistic approach to learning ICT.
+                      They brought me to the SST Open House and the rest is history.
+                      Deets on SST Open House? Here
+            </Text></View>
             </ScrollView>
           </SafeAreaView>
 
