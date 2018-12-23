a64fa078575eeb260a5216ba776a595422a362f9
AakashSanjayMehta <asrm003@gmail.com>
2018-12-23 23:40:29 +0700

    g

--- src/settlingIn.js
+++ src/settlingIn.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image, ImageBackground } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import { FILE_URL } from './constants'
 
@@ -9,8 +9,75 @@ class settlingIn extends Component {
   }
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={{
+        flex: 1,
+        backgroundColor: 'black'
+      }}>
+        <ImageBackground source={{ uri: FILE_URL + '/background.png' }}
+          style={{
+            flex: 1, resizeMode: 'contain'
+          }} imageStyle={{ opacity: 0.5 }}>
+
+          <SafeAreaView style={{
+            flex: 1,
+            margin: 16
+          }}>
+            <View>
+              <Image />
+              <Text style={{
+                fontFamily: 'Avenir Next',
+                textAlign: 'center',
+                fontSize: 30,
+                fontWeight: '600',
+                color: 'white'
+              }}>Rachel</Text>
+            </View>
+            <View style={{
+
+              width: '100%',
+              backgroundColor: 'white',
+              shadowColor: 'gray',
+              shadowBlur: 10,
+              shadowOffset: {
+                width: 2,
+                height: 2
+              },
+              shadowOpacity: 1,
+              borderRadius: 5,
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: 16
+            }}><Text style={{
+              padding: 8,
+              fontSize: 18,
+              fontFamily: 'Avenir Next',
+            }}>
+            </Text></View>
+          </SafeAreaView>
+
+
+
+
+
+          <SafeAreaView style={{
+            marginLeft: 8,
+            position: 'absolute',
+          }}>
+            <TouchableOpacity onPress={() => {
+              this.props.navigation.goBack()
+            }}>
+              <Icon name={'keyboard-arrow-left'} color={'white'} size={40} />
+            </TouchableOpacity>
+
+          </SafeAreaView>
+
+
+
+
+
+        </ImageBackground>
+
+
       </View>
     )
   }
